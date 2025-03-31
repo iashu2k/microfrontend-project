9be236412c6515e9e9a06575c16d4e0d2637262b
Ashutosh Mishra <iashu2k@gmail.com>
2025-03-31 22:15:15 +0530

    added auth to container federation plugin

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -19,6 +19,13 @@ const prodConfig = {
       },
       shared: packageJson.dependencies,
     }),
+    new ModuleFederationPlugin({
+      name: "auth",
+      remotes: {
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
   ],
 };
 
